904163db1151493691e750c83b473463dd6de20e
Harsh Bhandari <harshbhandari32@gmail.com>
2021-09-17 23:32:47 +0530

    Feat: Profile template and route

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -20,3 +22,7 @@ def register(request):
 		'users/register.html',
 		{ 'form': form },
 	)
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
