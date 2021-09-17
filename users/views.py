5ed296b4d3474013c4f616a389a57530ddf64646
Harsh Bhandari <harshbhandari32@gmail.com>
2021-09-17 22:28:44 +0530

    Feat: logout template

--- users/views.py
+++ users/views.py
@@ -12,8 +12,8 @@ def register(request):
 			username = form.cleaned_data.get('username')
 			# for displaying msg alerts
 			messages.success(request,
-				f'Account created for {username}!')
-			return redirect('blog-home')
+				f'Your account has been created! You can now login!')
+			return redirect('login')
 	else:
 		form = UserRegistrationForm()
 	return render(request,
