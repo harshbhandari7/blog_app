814f418ae7516003679e2608b2fa2decc370a9f1
Harsh Bhandari <harshbhandari32@gmail.com>
2021-09-23 17:49:23 +0530

    Feat: profile creation on user registration using signals

--- users/models.py
+++ users/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 # profile model
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
