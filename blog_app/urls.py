29952db45f274ec74ecd4fa3a3d0ce1e072162ef
Harsh Bhandari <harshbhandari32@gmail.com>
2021-09-24 19:50:09 +0530

    Feat: created list view for blog posts

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
+from .views import BlogListView
 
 urlpatterns = [
-	path('', views.home, name='blog-home'),
+	path('', BlogListView.as_view(), name='blog-home'),
 	path('about/', views.about, name='blog-about')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Post
 
 def home(request):
@@ -7,6 +9,13 @@ def home(request):
 		'blog_posts': Post.objects.all(),  #objects stored in the data base
 	}
 	return render(request, 'blog_app/home.html', context)
+
+# posts list view
+class BlogListView(ListView):
+	model = Post
+	template_name = 'blog_app/home.html'  # <app_name>/<model>_<view_type>.html
+	context_object_name = 'blog_posts'
+	ordering = ['-date_posted']
 
 def about(request):
 	return render(request, 'blog_app/about.html', { 'title': 'About' })
